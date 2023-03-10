16afb76f067de7c9aed078cd323a84ce5c33e8c2
gilyazov <gilyazov.aydar@gmail.com>
2023-03-10 14:37:44 +0300

    init

---
+++ urlrewrite.php
@@ -0,0 +1,19 @@
+<?php
+$arUrlRewrite=array (
+  0 => 
+  array (
+    'CONDITION' => '#^/rest/#',
+    'RULE' => '',
+    'ID' => NULL,
+    'PATH' => '/bitrix/services/rest/index.php',
+    'SORT' => 100,
+  ),
+  1 => 
+  array (
+    'CONDITION' => '#^/#',
+    'RULE' => NULL,
+    'ID' => 'bitrix:landing.pub',
+    'PATH' => '/index.php',
+    'SORT' => 100,
+  ),
+);
