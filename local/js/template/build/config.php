2e06c270efea9427ecf32ccfc47cd89a0bbf2105
gilyazov <gilyazov.aydar@gmail.com>
2023-03-10 15:02:02 +0300

    front

---
+++ local/js/template/build/config.php
@@ -0,0 +1,19 @@
+<?
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+{
+    die();
+}
+
+return [
+    'css' => [
+        "./assets/css/style.min.css",
+    ],
+    'js' => [
+        "./assets/js/main.js",
+        "./assets/js/backend.js",
+
+    ],
+    /*'rel' => [
+        'jquery3'
+    ]*/
+];
